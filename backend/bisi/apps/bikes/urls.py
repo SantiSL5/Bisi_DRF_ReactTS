913f894cf72c6bb861f39fb31466e61e630b2471
SantiSL5 <santiagosolerllin@gmail.com>
2023-02-01 19:29:01 +0100

    Delete many bikes

--- backend/bisi/apps/bikes/urls.py
+++ backend/bisi/apps/bikes/urls.py
@@ -3,5 +3,6 @@ from .view import BikeView
 
 urlpatterns = [
     path('', BikeView.as_view({'get':'getAllBikes', 'post': 'createBike'})),
+    path('deleteMany', BikeView.as_view({'post': 'deleteBikes'})),
     path('<int:id>', BikeView.as_view({'put': 'updateBike', 'delete': 'deleteBike'})),
 ]
--- backend/bisi/apps/bikes/view.py
+++ backend/bisi/apps/bikes/view.py
@@ -1,10 +1,12 @@
 from rest_framework.generics import get_object_or_404
 from django.template import context
 from .serializers import BikeSerializer
+from ..slots.serializers import SlotSerializer
 # from rest_framework.exceptions import NotFound
 from rest_framework import generics, mixins, status, viewsets
 from rest_framework.response import Response 
 from .models import Bike
+from ..slots.models import Slot
 from rest_framework.permissions import (AllowAny, IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser,)
 
 class BikeView(viewsets.GenericViewSet):
@@ -53,3 +55,18 @@ class BikeView(viewsets.GenericViewSet):
         bike = get_object_or_404(Bike.objects.all(),id=id)
         bike.delete()
         return Response({'data': 'Bike deleted'})
+    
+    def deleteBikes(self, request):
+        serializer_context = {
+            'ids': request.data['ids'],
+            'request': request
+        }
+
+        if len(serializer_context['ids']) > 0:
+            bikes,serializer = BikeSerializer.getBikesDelete(serializer_context)
+            if len(serializer) != len(serializer_context['ids']):
+                return Response({'data': "Some bikes doesn't exist"})
+            bikes.delete()
+            return Response({'data': 'Bikes deleted'})
+        return Response({'data': 'No bikes provided'})
+
